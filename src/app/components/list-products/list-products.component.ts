4278442801667e810f12fea11d6d74c112bc8c8b
edwinkay <edwinkaycut@gmail.com>
2023-04-25 00:40:10 -0500

    modal eliminar

--- src/app/components/list-products/list-products.component.ts
+++ src/app/components/list-products/list-products.component.ts
@@ -14,12 +14,14 @@ export class ListProductsComponent implements OnInit {
   //creando un array
   productos: any[] = [];
   sumas: any[] = [];
-  total: number;
+  total: number = 0;
   all: any[] = [];
 
   displayModal: boolean = false;
+  displayModal2: boolean = false;
   valorRestado: any;
   resultado: number = 0;
+  buttonDisabled: boolean;
 
   // items: Observable<any[]>;
 
@@ -38,6 +40,12 @@ export class ListProductsComponent implements OnInit {
   //metodo añadido desde el servicio
   getProducts() {
     this._services.getProducts().subscribe((data) => {
+      if (data.length === 0) {
+        this.buttonDisabled = true
+        this.total = 0;
+      }else {
+        this.buttonDisabled = false
+      }
       this.productos = [];
       data.forEach((element: any) => {
         this.productos.push({
@@ -49,7 +57,6 @@ export class ListProductsComponent implements OnInit {
           0
         );
         this.total = total;
-        console.log(total);
       });
     });
   }
@@ -62,19 +69,32 @@ export class ListProductsComponent implements OnInit {
           'El producto fue eliminado con exito',
           'Producto eliminado'
         );
+        this.valorRestado = null;
+        this.resultado = 0;
         console.log('deleted...');
       })
       .catch((error) => {
         console.log(error);
       });
   }
+  borrarTodo(){
+    this._services.deleteAllProducts()
+    this.displayModal2 = false;
+    this.total = 0;
+    this.valorRestado = null;
+    this.resultado = 0
+  }
 
   showDialog() {
     this.displayModal = true;
   }
+  showDialog2() {
+    this.displayModal2 = true;
+  }
 
   onDialogHide() {
     this.displayModal = false;
+    this.displayModal2 = false;
   }
   restarValor() {
     if (this.valorRestado >= this.total) {
@@ -82,6 +102,6 @@ export class ListProductsComponent implements OnInit {
     }else{
       this.resultado = 0
     }
-
   }
+
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,52 +3,62 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
-  constructor(private firestore: AngularFirestore) { }
-
+  constructor(private firestore: AngularFirestore) {}
 
   //envia desde el html a firebase
-  agregarproducto(producto: any): Promise<any>{
-    return this.firestore.collection('productos').add(producto)
+  agregarproducto(producto: any): Promise<any> {
+    return this.firestore.collection('productos').add(producto);
   }
 
   //consumir datos, obteniendo desde firebase
   //metos orderBy para ordenar
-  getProducts(): Observable<any>{
-    return this.firestore.collection('productos', ref => ref.orderBy('table', 'asc')).snapshotChanges()
+  getProducts(): Observable<any> {
+    return this.firestore
+      .collection('productos', (ref) => ref.orderBy('table', 'asc'))
+      .snapshotChanges();
   }
 
   //eliminar productos
-  deleteProducts(id: string): Promise<any>{
-    return this.firestore.collection('productos').doc(id).delete()
+  deleteProducts(id: string): Promise<any> {
+    return this.firestore.collection('productos').doc(id).delete();
+  }
+  deleteAllProducts(): void {
+    const productosRef = this.firestore.collection('productos');
+    const productos$: Observable<any[]> = productosRef.snapshotChanges();
+
+    productos$.subscribe((productos) => {
+      productos.forEach((producto) => {
+        this.firestore
+          .collection('productos')
+          .doc(producto.payload.doc.id)
+          .delete();
+      });
+    });
   }
   //peticion para cargar los datos en editar productos
-  getProducto(id: string): Observable<any>{
-    return this.firestore.collection('productos').doc(id).snapshotChanges()
+  getProducto(id: string): Observable<any> {
+    return this.firestore.collection('productos').doc(id).snapshotChanges();
   }
   //peticion de los datos editados (actualizarlos)
-  update(id: string, data:any): Promise<any>{
-    return this.firestore.collection('productos').doc(id).update(data)
+  update(id: string, data: any): Promise<any> {
+    return this.firestore.collection('productos').doc(id).update(data);
   }
-
-
-
 
   //creando instancia para los ajustes
 
-  ajustarProducto(ajustes: any): Promise<any>{
-    return this.firestore.collection('ajustes').add(ajustes)
+  ajustarProducto(ajustes: any): Promise<any> {
+    return this.firestore.collection('ajustes').add(ajustes);
   }
-  obtenerAjuste(id: string): Observable<any>{
-    return this.firestore.collection('ajustes').doc(id).snapshotChanges()
+  obtenerAjuste(id: string): Observable<any> {
+    return this.firestore.collection('ajustes').doc(id).snapshotChanges();
   }
-  obtProductos(): Observable<any>{
-    return this.firestore.collection('ajustes').snapshotChanges()
+  obtProductos(): Observable<any> {
+    return this.firestore.collection('ajustes').snapshotChanges();
   }
-  updatex(id: string, data:any): Promise<any>{
-    return this.firestore.collection('ajustes').doc(id).update(data)
+  updatex(id: string, data: any): Promise<any> {
+    return this.firestore.collection('ajustes').doc(id).update(data);
   }
 }
